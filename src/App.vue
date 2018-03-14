1fe82fdb01154f7a56a74457ecc6c1093a3fd2ea
Chris Schweda <cschweda@gmail.com>
2018-03-14 15:25:50 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,293 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <div class="navHeader d-none d-lg-block">
+
+        <h1>Adult
+          <br>Redeploy
+          <br>Illinois</h1>
+        <h2>2018 All Sites Summit</h2>
+
+      </div>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">ARI 2018 All Sites Summit</span>
+
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#home'">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#about-summit'">About the Summit</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#about-ari'">About Adult Redeploy Illinois</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#about-cjcc'">About Criminal Justice Coordinating Councils</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#agenda'">Agenda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#registration'">Registration, Lodging & Meals</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" v-scroll-to="'#resources'">Resources</a>
+          </li>
+          <li class="navFooter d-none d-lg-block">
+            <img src="./assets/icjia.png" alt="ICJIA Logo" class="navFooterLogo" />
+            <div class="navFooterCopy">&copy;2018 Illinois Criminal Justice Information Authority</div>
+          </li>
+        </ul>
+      </div>
+
+    </nav>
+
+
+    <router-view/>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    methods: {
+      scrollToTarget: function (e) {
+        console.log(e.target.getAttribute('href'))
+      }
+
+    },
+    mounted() {
+
+
+      // Closes responsive menu when a scroll trigger link is clicked
+      $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+      });
+
+      // Activate scrollspy to add active class to navbar items on scroll
+      $('body').scrollspy({
+        target: '#sideNav'
+      });
+
+    }
+  }
+</script>
+
+<style>
+  :root {
+    --main-bg-color: #068587;
+  }
+
+
+
+  body {
+    font-family: 'Open Sans', serif;
+    padding-top: 54px;
+    color: #333;
+    font-size: 18px;
+  }
+
+  @media (min-width:992px) {
+    body {
+      padding-top: 0;
+      padding-left: 17rem
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'Saira Extra Condensed', serif;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #343a40
+  }
+
+  h1 {
+    font-size: 6rem;
+    line-height: 5.5rem
+  }
+
+  h2 {
+    font-size: 3.5rem
+  }
+
+  .subheading {
+    text-transform: uppercase;
+    font-weight: 500;
+    font-family: 'Saira Extra Condensed', serif;
+    font-size: 1.35rem
+  }
+
+
+
+  #sideNav .navbar-nav .nav-item .nav-link {
+    font-weight: 600;
+    text-transform: uppercase
+  }
+
+  @media (min-width:992px) {
+    #sideNav {
+      text-align: center;
+      position: fixed;
+      top: 0;
+      left: 0;
+      display: flex;
+      flex-direction: column;
+      width: 17rem;
+      height: 100vh;
+      overflow: auto;
+    }
+    #sideNav .navbar-brand {
+      display: flex;
+      margin: auto auto 0;
+      padding: .5rem
+    }
+    #sideNav .navbar-brand .img-profile {
+      max-width: 10rem;
+      max-height: 10rem;
+      border: .5rem solid rgba(255, 255, 255, .2)
+    }
+    #sideNav .navbar-collapse {
+      display: flex;
+      align-items: flex-start;
+      flex-grow: 0;
+      width: 100%;
+      margin-bottom: auto
+    }
+    #sideNav .navbar-collapse .navbar-nav {
+      flex-direction: column;
+      width: 100%;
+
+    }
+    #sideNav .navbar-collapse .navbar-nav .nav-item {
+      display: block
+    }
+    #sideNav .navbar-collapse .navbar-nav .nav-item .nav-link {
+      display: block
+    }
+
+    .image-container {
+      margin-top: -120px;
+
+    }
+  }
+
+  section.content-section {
+    border-bottom: 1px solid #dee2e6;
+    padding-top: 5rem!important;
+    padding-bottom: 5rem!important
+  }
+
+
+
+  @media (min-width:768px) {
+    section.content-section {
+      min-height: 100vh;
+
+    }
+
+    .navHeader {
+      border-bottom: 1px solid #aaa;
+      padding-bottom: 30px;
+    }
+    .navHeader h1 {
+      margin-top: 20px !important;
+      font-size: 72px !important;
+      line-height: 68px !important;
+      color: #fff;
+    }
+
+    .navHeader h2 {
+      margin-top: 20px !important;
+      font-size: 22px !important;
+      line-height: 22px !important;
+      color: #ccc;
+    }
+
+    .navFooter {
+      padding-top: 30px;
+    }
+
+    .navFooterLogo {
+      width: 75px;
+      padding-bottom: 15px !important;
+      padding-top: 30px !important;
+    }
+
+    .navFooterCopy {
+      font-size: 9px;
+      color: #eee;
+      padding-bottom: 15px !important;
+    }
+  }
+
+  @media (min-width:992px) {
+    section.content-section {
+      padding-top: 3rem!important;
+      padding-bottom: 3rem!important
+    }
+
+    section.content-section.first {
+
+      padding-top: 10rem!important;
+
+    }
+  }
+
+  .bg-primary {
+    background-color: #068587 !important;
+  }
+
+
+  a {
+    color: #068587
+  }
+
+  a:active,
+  a:focus,
+  a:hover {
+    color: #ccc;
+    cursor: pointer;
+  }
+
+
+  #home {}
+
+  #about-summit {
+    background: #35999B;
+    color: white;
+  }
+
+  #about-summit h2 {
+    color: #fff;
+  }
+
+  #about-ari {}
+
+  #about-cjcc {
+    background: #35999B;
+    color: white;
+
+  }
+
+  #about-cjcc h2 {
+    color: #fff;
+  }
+
+  .primary {
+    color: #068587 !important;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="hello">
+    <div class="container-fluid p-0">
+
+      <section class="content-section first p-3 p-lg-5" id="home">
+        <div class="text-center">
+          <div>
+            <h1 class="mb-5">2018
+              <span class="primary">Adult Redeploy Illinois</span>
+            </h1>
+            <h2 class="muted">All-Sites Summit</h2>
+            <h3>May 9-10, 2018</h3>
+            <p>DoubleTree by Hilton, Bloomington</p>
+            <p> Illinois 10 Brickyard Drive</p>
+            <p>Bloomington, IL 61701</p>
+
+          </div>
+        </div>
+      </section>
+
+      <section class="content-section p-3 p-lg-5 d-flex flex-column" id="about-summit">
+        <div class="my-auto">
+          <h2 class="mb-5">About the Summit</h2>
+
+          <div class="d-flex flex-column flex-md-row mb-5">
+            <div class="mr-auto">
+              <p>The seventh annual ARI All-Sites Summit will focus on understanding, communicating, and expanding our impact.
+                This year, ARI is partnering with the Criminal Justice Coordinating Councils (CJCC) to bring an even richer
+                Summit experience to our sites and stakeholders, including an extra half-day of optional programming hosted
+                by the CJCCs. CJCCs are partnerships of local criminal justice stakeholders, service providers, and community
+                partners, often organized on the county level, that engage in collaborative criminal justice planning. We
+                expect that sharing space at the Summit will provide many opportunities for mutual learning.</p>
+              <p>The Summit will include a day and a half of panels, breakout sessions, technical training, and networking.
+                ARI sites are also invited and encouraged to attend the optional pre-summit workshops in conjunction with
+                CJCCs from around the state, on the afternoon of May 8th. The CJCC-focused sessions will include presentations
+                on how to develop and implement strategic plans, as well as highlight projects undertaken by current CJCC
+                partners (In McLean, McHenry, Lake, Winnebago, and St. Clair Counties) to improve their local criminal justice
+                systems.
+              </p>
+              <p> Those expected to attend are a spectrum of ARI and CJCC site stakeholders including but not limited to judges,
+                prosecutors, defense attorneys, treatment providers, probation staff and supervisors, law enforcement, program
+                coordinators, researchers, program partners, and policymakers. Summit content is structured for relevancy
+                to a broad group of stakeholders. In addition to current ARI program representation, planning grantees are
+                also invited and have previously benefited from talking with current sites and accessing system-wide training.
+                The Illinois Center of Excellence for Behavioral Health and Justice will provide CEUs for LPC, LCPC, LSW,
+                LCSW, LMFT, Psychologists, IAODAPCA, and CLEs for attorneys. CEUs will be provided for all three days.</p>
+
+
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+
+      </section>
+
+      <section class="content-section p-3 p-lg-5 d-flex flex-column" id="about-ari">
+        <div class="my-auto">
+          <h2 class="mb-5 primary">About Redeploy Illinois</h2>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-sm">
+                <p>Adult Redeploy Illinois is a state initiative providing funding and technical assistance to local jurisdictions
+                  to support the diversion of non-violent offenders from prison to more effective and less expensive community-based
+                  supervision and services.</p>
+                <p>ARI sites use grant funds to design and implement local programs that address offenders' risks and needs
+                  and leverage their assets (family support, employment) to improve public safety and offender outcomes.
+                  ARI has grown from five initial pilot sites in early 2011, to the current 22 sites implementing 45 programs
+                  covering 45 Illinois counties (as of March 2018).</p>
+
+              </div>
+              <div class="col-sm">
+                <div class="image-container">
+                  <img src="../assets/map.png" width="450" class="img-fluid" />
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+
+      <section class="content-section p-3 p-lg-5 d-flex flex-column" id="about-cjcc">
+        <div class="my-auto">
+          <h2 class="mb-5">About Criminal Justice Coordinating Councils</h2>
+
+
+          <div class="d-flex flex-column flex-md-row mb-5">
+            <div class="mr-auto">
+              <p>Criminal Justice Coordinating Councils (CJCCs) are partnerships of decision makers who have a stake in the
+                effective administration of justice. Typically the partnerships are organized at the county level and include
+                representatives from:</p>
+              <ul>
+                <li>The 3 branches of government (executive, judicial, and legislative);</li>
+                <li>Multiple levels of government (city, county, and state agencies);</li>
+                <li>and " Allied stakeholders from various other governmental entities, such as education or health and human
+                  services, community based organizations, service providers, and citizens.</li>
+              </ul>
+              <p>
+                CJCCs follow a structured, data-driven planning process to identify, analyze, solve, and manage justice system issues. This
+                work leads to better understanding of crime and criminal justice problems, greater cooperation among agencies
+                and units of local government, clearer objectives and priorities, more effective resource allocation, and
+                better quality criminal justice programs and personnel. Taken together, these results can increase public
+                confidence in and support for criminal justice processes, enhancing system performance and, ultimately, the
+                integrity of the law.</p>
+
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+
+      <section class="content-section p-3 p-lg-5 d-flex flex-column" id="agenda">
+        <div class="my-auto">
+          <h2 class="mb-5 primary">Agenda</h2>
+          <div class="d-flex flex-column flex-md-row mb-5">
+            <div class="mr-auto">
+              <p>The 2018 ARI All-Sites Summit will offer opportunities to share site updates and lessons learned, skills-building
+                for communicating and expanding your ARI programs' impact, grants technical assistance, and more.</p>
+              <p>For more information, contact emma.rubin@illinois.gov, Policy and Program Coordinator, Adult Redeploy Illinois.</p>
+              <p>Agenda coming soon</p>
+
+
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    mounted() {
+      console.log('mounted')
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
